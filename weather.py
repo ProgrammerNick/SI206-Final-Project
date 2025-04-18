556f4a2cf1501d199f85baa16809d6f2967dfac9
Conner <connerh777@gmail.com>
2025-04-18 16:53:18 -0400

    Made changes for the import

--- weather.py
+++ weather.py
@@ -4,6 +4,9 @@ import os
 import requests
 import re
 from collections import Counter
+import matplotlib
+import matplotlib.pyplot as plt
+import time
 
 API_KEY = 'GLVTG3EDCK29FQPYLQLS6Q3F4'
 
@@ -56,8 +59,8 @@ def search_for_weather(city):
     response = requests.get(url, params=querystring)
 
     if response.status_code == 200:
-        with open("weather.json", 'w', encoding="utf-8") as f:
-            json.dump(response.json(), f, indent=4)
+        #with open("weather.json", 'w', encoding="utf-8") as f:
+            #json.dump(response.json(), f, indent=4)
         return response.json()
     else:
         return f"Error {response.status_code}"
@@ -123,8 +126,8 @@ def create_weather_table(data, cur, conn, days):
                     VALUES (?,?,?,?,?,?,?,?,?,?)""", 
                     (weather_id, city_id, temp, humidity, wind_speed, uv_index, chance_of_precip,
                      conditions_id, date, time))
-        print((weather_id, city_id, humidity, wind_speed, uv_index, chance_of_precip,
-                conditions_id, date, time))
+        #print((weather_id, city_id, humidity, wind_speed, uv_index, chance_of_precip,
+        #        conditions_id, date, time))
         weather_id += 1
     conn.commit()
 
@@ -144,13 +147,22 @@ def create_cities_table(cur, conn):
             cur.execute("INSERT OR IGNORE INTO Cities (id, city) VALUES (?,?)", (i, lines[i]))
         conn.commit()
 
-def daily_high_and_low(cur, day, city):
+def daily_high_and_low(cur, date, city):
     cur.execute("SELECT id from Cities WHERE city = ?", (city,))
     city_id = int(cur.fetchone()[0])
-    cur.execute("SELECT MIN(temperature) from Weather WHERE city_id = ? AND date = ?", (city_id, day))
+    cur.execute("SELECT MIN(temperature) from Weather WHERE city_id = ? AND date = ?", (city_id, date))
     min_temp = int(cur.fetchone()[0])
-    cur.execute("SELECT MAX(temperature) from Weather WHERE city_id = ? AND date = ?", (city_id, day))
+    cur.execute("SELECT MAX(temperature) from Weather WHERE city_id = ? AND date = ?", (city_id, date))
     max_temp = int(cur.fetchone()[0])
+    with open("Calculations.txt", 'w') as f:
+        lines = []
+        date_str = str(date)
+        year = date_str[0:4]
+        month = date_str[5:6]
+        day = date_str[-2:-1]
+        lines.append(f"Maximum Temperature for {month}/{day}/{year} in {city}: {max_temp}.")
+        lines.append(f"Minimum Temperature for {month}/{day}/{year} in {city}: {min_temp}.")
+        f.writelines(lines)
     return max_temp, min_temp
 
 def daily_avg(cur, day, city):
@@ -171,21 +183,56 @@ def daily_avg(cur, day, city):
     conditions = cur.fetchone()[0]
     return temp, humidity, wind_speed, uv_index, precip, conditions
 
+def create_avg_chart(cur, city, data):
+    cur.execute("""SELECT DISTINCT Weather.date, Cities.city 
+                FROM Weather 
+                JOIN Cities ON Weather.city_id = Cities.id
+                WHERE Cities.city = ?""", (city,))
 
+    dates = [row[0] for row in cur.fetchall()]
+    temp = [row[0] for row in data]
+    humidity = [row[1] for row in data]
+    print(dates)
+    print(temp)
+    fig, ax1 = plt.subplots()
 
-def daily_feels_like_data(cur, day, city):
-    pass
+    ax1.plot(dates, temp, 'r-', label='Temperature (°F)')
+    ax1.set_ylabel('Temperature (°F)', color='r')
+    ax1.tick_params(axis='y', labelcolor='r')
 
-def main():
+    ax1.set_xlabel("Date")
+
+    ax2 = ax1.twinx()
+
+    # Plot humidity on the right Y-axis
+    ax2.plot(dates, humidity, 'b--', label='Humidity (%)')
+    ax2.set_ylabel('Humidity (%)', color='b')
+    ax2.tick_params(axis='y', labelcolor='b')
+
+    plt.title(f'Weather Trends in {city}')
+
+    plt.show()
+
+"""def main():
     weather_dict = search_for_weather("Ann Arbor")
     cur, conn = setup_weather_database("weather.db")
     create_conditions_table(cur, conn)
     create_cities_table(cur, conn)
-    create_weather_table(weather_dict, cur, conn, 1)
-    print(daily_avg(cur, 20250415, "Ann Arbor"))
-    print(daily_high_and_low(cur, 20250415, "Ann Arbor"))
+    averages = []
+    high_and_low = []
+    date = 20250416
+    for i in range(5):
+        print(date)
+        create_weather_table(weather_dict, cur, conn, i)
+        averages.append(daily_avg(cur, date, "Ann Arbor"))
+        high_and_low.append(daily_high_and_low(cur, date, "Ann Arbor"))
+        date += 1
+        time.sleep(2)
+    print(averages)
+    print(high_and_low)
+    create_avg_chart(cur, "Ann Arbor", averages)
 
     #print(weather_dict)
 
 if __name__ == "__main__":
-    main()
+    main()"""
